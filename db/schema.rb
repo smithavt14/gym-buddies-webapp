be95223c5acb11f16258dc40004fde481de96e9e
Calvin Cheung <calvchum@gmail.com>
2018-07-31 14:47:15 +0800

    fixing database

--- db/schema.rb
+++ db/schema.rb
@@ -13,39 +13,3 @@
 ActiveRecord::Schema.define(version: 2018_07_31_023439) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "bookings", force: :cascade do |t|
-    t.bigint "workout_id"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_bookings_on_user_id"
-    t.index ["workout_id"], name: "index_bookings_on_workout_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "location"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "workouts", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "location"
-    t.integer "max_participants"
-    t.datetime "time"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_workouts_on_user_id"
-  end
-
-  add_foreign_key "bookings", "users"
-  add_foreign_key "bookings", "workouts"
-  add_foreign_key "workouts", "users"
-end
