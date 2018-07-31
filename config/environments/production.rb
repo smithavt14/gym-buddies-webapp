53a0d1f86614773313263186aa89872b4a1e10e8
Alex Smith <smithavt14@gmail.com>
2018-07-31 16:11:14 +0800

    Removed the YUI compressor from the production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,8 +31,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.assets.css_compressor = :yui
-  config.assets.js_compressor = :uglifier
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
