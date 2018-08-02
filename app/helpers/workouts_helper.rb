00a5ec58ab8db18c52d440e720eadcf8592cc3a1
Calvin Cheung <calvchum@gmail.com>
2018-08-02 23:36:25 +0800

    created helper that makes placeholder images, linked the avatar to user_path

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -1,2 +1,14 @@
 module WorkoutsHelper
+
+  def placeholder(thing, image_placeholder)
+    thing.photo.present? ? cl_image_path(thing.photo, height: 500, width: 800, crop: :fill) : image_path(image_placeholder)
+  end
+
+  def workout_placeholder(workout)
+    placeholder(workout, 'placeholder.jpg')
+  end
+
+  def user_placeholder(workout)
+    placeholder(workout.user, 'profile_placeholder.svg')
+  end
 end
